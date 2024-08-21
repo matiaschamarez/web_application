26c41e72cad8970122c28f541bef4c494147eff6
mati <matiaslchamarez@gmail.com>
2024-08-21 19:41:48 -0300

    agregue Character.cs y CharacterController.cs. Ademas agregue un json que dentro hay una lista con los datos para usarlos en el swagger

---
+++ web_application/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace web_application
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string Range { get; set; }
+        public string Type { get; set; }
+        public string HardAbility { get; set; }
+    }
+}
---
+++ web_application/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace web_application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        // POST api/<CharacterController>
+        [HttpPost]
+        public IActionResult PostCharacters([FromBody] List<Character> characters)
+        {
+            if (characters == null || characters.Count == 0)
+            {
+                return BadRequest("No characters provided.");
+            }
+
+            // Procesa la lista de personajes
+            return Ok(characters);
+        }
+
+    }
+}
